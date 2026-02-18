88fdb62362e4ed84fc19e7bb7835630b75b8a3bb
hiteshhitti <hitesh.hitti@gmail.com>
2026-02-18 23:55:49 +0530

    tenth commit

--- HotelManagementSystem/settings.py
+++ HotelManagementSystem/settings.py
@@ -17,8 +17,14 @@ import os
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
-    ...
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -43,6 +49,16 @@ ALLOWED_HOSTS = ['*']
 
 # Application definition
 
+# INSTALLED_APPS = [
+#     'django.contrib.admin',
+#     'django.contrib.auth',
+#     'django.contrib.contenttypes',
+#     'django.contrib.sessions',
+#     'django.contrib.messages',
+#     'django.contrib.staticfiles',
+#     'HotelApp'
+# ]
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -50,18 +66,18 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'HotelApp'
+    'HotelApp',
 ]
 
-MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-]
+# MIDDLEWARE = [
+#     'django.middleware.security.SecurityMiddleware',
+#     'django.contrib.sessions.middleware.SessionMiddleware',
+#     'django.middleware.common.CommonMiddleware',
+#     'django.middleware.csrf.CsrfViewMiddleware',
+#     'django.contrib.auth.middleware.AuthenticationMiddleware',
+#     'django.contrib.messages.middleware.MessageMiddleware',
+#     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+# ]
 
 ROOT_URLCONF = 'HotelManagementSystem.urls'
 
@@ -139,17 +155,24 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS =[
-  os.path.join (BASE_DIR / 'static')
+# STATICFILES_DIRS =[
+#   os.path.join (BASE_DIR / 'static')
+# ]
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
 ]
-STATIC_ROOT = os.path.join (BASE_DIR,'assets')
-MEDIA_ROOT = os.path.join (BASE_DIR,'static/Media')
-MEDIA_URL = '/Media/'
+
+# STATIC_ROOT = os.path.join (BASE_DIR,'assets')
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+# MEDIA_ROOT = os.path.join (BASE_DIR,'static/Media')
+MEDIA_ROOT = BASE_DIR / "media"
+MEDIA_URL = '/media/'
 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-DEBUG = True
